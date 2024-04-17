d18fbdceba04cc07a03810788acb65a036c1d0a5
mrinmoygh081 <mrinmoygh081@gmail.com>
2024-04-17 13:09:54 +0530

    17-04-2024

--- BCA2024A/reactintro/src/pages/About.jsx
+++ BCA2024A/reactintro/src/pages/About.jsx
@@ -1,8 +1,42 @@
-import React from "react";
-import SideBar from "../components/SideBar";
+import React, { useEffect, useState } from "react";
 
 const About = () => {
-  return <>dkjvkfldsjfl;</>;
+  const [data, setData] = useState(null);
+  const [reload, setReload] = useState(false);
+  console.log(data);
+
+  const getData = () => {
+    const requestOptions = {
+      method: "GET",
+    };
+
+    fetch("https://jsonplaceholder.typicode.com/posts", requestOptions)
+      .then((response) => response.json())
+      .then((result) => setData(result))
+      .catch((error) => console.error(error));
+  };
+
+  //
+  useEffect(() => {
+    getData();
+  }, [reload]);
+
+  return (
+    <>
+      <button className="btn btn-primary" onClick={() => setReload(!reload)}>
+        RERENDER PLEASE
+      </button>
+      {data &&
+        data.map((item, index) => {
+          return (
+            <div className="card p-3 mb-2" key={index}>
+              <h3>{item.title}</h3>
+              <p>{item.body}</p>
+            </div>
+          );
+        })}
+    </>
+  );
 };
 
 export default About;
--- BCA2024A/reactintro/src/pages/Contact.jsx
+++ BCA2024A/reactintro/src/pages/Contact.jsx
@@ -1,8 +1,59 @@
-import React from "react";
-import SideBar from "../components/SideBar";
+import React, { useState } from "react";
 
 const Contact = () => {
-  return <>Contact</>;
+  const [data, setData] = useState({
+    email: "",
+    phone: "",
+    message: "",
+  });
+
+  console.log("this is data:", data);
+
+  return (
+    <>
+      <div className="container">
+        <form>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+              Email
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              placeholder="Eg: name@example.com"
+              onChange={(e) => setData({ ...data, email: e.target.value })}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="phone" className="form-label">
+              Phone
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="phone"
+              onChange={(e) => setData({ ...data, phone: e.target.value })}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="message" className="form-label">
+              Your Message
+            </label>
+            <textarea
+              name=""
+              id=""
+              cols="30"
+              rows="10"
+              className="form-control"
+              onChange={(e) => setData({ ...data, message: e.target.value })}
+            ></textarea>
+          </div>
+          <button className="btn btn-success">SUBMIT</button>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default Contact;
